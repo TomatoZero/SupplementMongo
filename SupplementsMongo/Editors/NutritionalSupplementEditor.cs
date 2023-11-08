a62a9e0361b0db5642800f5a6befd7e2824bc66e
TOMATO\chima <chima05.2015@gmail.com>
2023-11-08 20:18:11 +0200

    feat(logic): NutritionaSupplementEditor implement

---
+++ SupplementsMongo/Editors/NutritionalSupplementEditor.cs
@@ -0,0 +1,33 @@
+﻿using NutritionalSupplements.Data;
+
+namespace SupplementsMongo.Editors;
+
+public static class NutritionalSupplementEditor
+{
+    private static NutritionalSupplementRepository _repository;
+
+    static NutritionalSupplementEditor()
+    {
+        _repository = new NutritionalSupplementRepository();
+    }
+
+    public static List<NutritionalSupplement> GetTable()
+    {
+        return _repository.GetAll().ToList();
+    }
+
+    public static void Update(NutritionalSupplement supplement)
+    {
+        _repository.Update(supplement);    
+    }
+
+    public static void Add(NutritionalSupplement supplement)
+    {
+        _repository.Add(supplement);
+    }
+
+    public static void Remove(NutritionalSupplement supplement)
+    {
+        _repository.Delete(supplement.Id);
+    }
+}
