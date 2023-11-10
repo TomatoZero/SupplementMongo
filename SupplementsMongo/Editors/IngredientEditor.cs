64c95b4de8a654e4a9ed8c219530b2570851c3f0
TOMATO\chima <chima05.2015@gmail.com>
2023-11-10 11:48:44 +0200

    feat(logic): implement IngredientEditor

---
+++ SupplementsMongo/Editors/IngredientEditor.cs
@@ -0,0 +1,38 @@
+﻿using NutritionalSupplements.Data;
+
+namespace SupplementsMongo.Editors;
+
+public static class IngredientEditor
+{
+    private static IngredientRepository _repository;
+
+    static IngredientEditor()
+    {
+        _repository = new IngredientRepository();
+    }
+
+    public static List<Ingredient> GetTable()
+    {
+        return _repository.GetAll().ToList();
+    }
+    
+    public static List<Ingredient> GetTableInclude()
+    {
+        return _repository.GetAllInclude().ToList();
+    }
+
+    public static void Update(Ingredient ingredient)
+    {
+        _repository.Update(ingredient);    
+    }
+
+    public static void Add(Ingredient ingredient)
+    {
+        _repository.Add(ingredient);
+    }
+
+    public static void Remove(Ingredient ingredient)
+    {
+        _repository.Delete(ingredient.Id);
+    }
+}
